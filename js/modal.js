75a0a7580259b0cd86f0189e71cd4815f25db308
JustinePham <tinemanalili@gmail.com>
2016-09-12 11:30:19 +0200

    added changes to modal. some js

--- js/modal.js
+++ js/modal.js
@@ -3,3 +3,16 @@ $('.campaign-card').on("click", function(){
     console.log(attribute);
     $('.modal-header').attr("style", attribute);
 });
+
+var campaignTitle = document.getElementById("campaignTitle").innerHTML;
+var companyName = document.getElementById("companyName").innerHTML;
+var totalAmt = document.getElementById("total").innerHTML;
+var antPost = document.getElementById("antPost").innerHTML;
+var minFollowers = document.getElementById("minFollowers").innerHTML;
+
+
+$('.company-name').append(companyName);
+$('.campaign-title').append(campaignTitle);
+$('.total-amount').append(totalAmt);
+$('.ant-post').append(antPost);
+$('.min-followers').append(minFollowers);
